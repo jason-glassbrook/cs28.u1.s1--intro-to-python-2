2b3b1c44e201624a9d0ef552ea335f6aeb085534
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-04-09 18:34:00 -0400

    write basic REPL functionality

--- src/adventure.py
+++ src/adventure.py
@@ -2,7 +2,9 @@
 #   ADVENTURE
 ############################################################
 
+import tools
 from room import Room
+from player import Player
 
 ########################################
 #   rooms
@@ -62,6 +64,8 @@ rooms['treasure'].s_to = rooms['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+player = Player('Jason', rooms['outside'])
+
 # Write a loop that:
 #
 # * Prints the current room name
@@ -72,3 +76,101 @@ rooms['treasure'].s_to = rooms['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters 'q', quit the game.
+
+
+def are_synonyms(thesaurus, *words):
+
+    for entry in thesaurus:
+
+        words_in_entry = False
+
+        for word in words:
+
+            if word in entry:
+
+                words_in_entry = True
+
+            else:
+
+                words_in_entry = False
+                break
+
+        if words_in_entry:
+
+            # all words in same entry -> they are synonyms
+            return True
+
+        # ... next!
+
+    # all words were never in the same entry -> they are not synonyms
+    return False
+
+
+# meta verbs with synonyms
+meta_verbs = (
+    ('q', 'quit'),
+)
+
+# direction verbs with synonyms
+direction_verbs = (
+    ('n', 'north'),
+    ('e', 'east'),
+    ('s', 'south'),
+    ('w', 'west'),
+)
+
+# all the verbs!
+verbs = tools.flatten((
+    *meta_verbs,
+    *direction_verbs,
+))
+
+########################################
+#   REPL
+########################################
+
+turns_count = 0
+
+while True:
+
+    # get user input
+    raw_user_input = input('> ').strip()
+    user_input = raw_user_input.lower()
+
+    # check validity of user input
+    if not user_input.startswith(verbs):
+
+        print(f'"{user_input}" is not a recognized command. Try again.')
+
+    else:
+
+        if user_input.startswith(tools.flatten(meta_verbs)):
+
+            if are_synonyms(meta_verbs, user_input, 'quit'):
+
+                print('Goodbye!')
+                break
+
+        elif user_input.startswith(tools.flatten(direction_verbs)):
+
+            if are_synonyms(direction_verbs, user_input, 'north'):
+
+                print('Moving north...')
+
+            if are_synonyms(direction_verbs, user_input, 'east'):
+
+                print('Moving east...')
+
+            if are_synonyms(direction_verbs, user_input, 'south'):
+
+                print('Moving south...')
+
+            if are_synonyms(direction_verbs, user_input, 'west'):
+
+                print('Moving west...')
+
+        else:
+
+            print('HOW DID YOU GET HERE? YOU SHOULD NOT BE HERE!')
+
+        turns_count += 1
